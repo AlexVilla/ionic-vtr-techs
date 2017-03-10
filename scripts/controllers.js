367eab51dcdc6c6152b85e98b6388b292d095ca8
Alex Villa <alex.villa.v@gmail.com>
2017-03-10 10:44:24 -0300

    camera plugin added

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -144,8 +144,28 @@ angular.module('techs')
     }
 }])
 
-.controller('TaskDetailCtrl', ['$scope', 'LoginService', '$state', '$ionicPopup', function($scope, LoginService, $state, $ionicPopup){
+.controller('TaskDetailCtrl', ['$scope', 'DetailService', '$state', '$ionicPopup', '$cordovaCamera', function($scope, LoginService, $state, $ionicPopup, $cordovaCamera){
 
+    var cameraOptions = {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: true,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 100,
+        targetHeight: 100,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false,
+        correctOrientation:true
+    };
+
+    $cordovaCamera.getPicture(cameraOptions).then(function(imageData) {
+        var image = document.getElementById('myImage');
+        image.src = "data:image/jpeg;base64," + imageData;
+    }, function(err) {
+        // error
+    });
+    
 }])
 ;
 
