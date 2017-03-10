dc72d841a8c349fba8bd6e0c9b95249415b0ddc4
Alex Villa <alex.villa.v@gmail.com>
2017-03-10 12:34:54 -0300

    camera

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -151,28 +151,14 @@ angular.module('techs')
             .then(function(imageData) {
                 console.log('camera data: ' + angular.toJson(imageData));
                 $scope.pictureUrl = "data:image/jpeg;base64," + imageData;
-                //var image = document.getElementById('myImage');
-                //image.src = "data:image/jpeg;base64," + imageData;
         }, function(err) {
                 console.log('camera error: ' + angular.toJson(err));
         });
     };
     var cameraOptions = {
-        //quality: 50,
         destinationType: Camera.DestinationType.DATA_URL,
-        //sourceType: Camera.PictureSourceType.CAMERA,
-        //allowEdit: true,
-        encodingType: Camera.EncodingType.JPEG,
-       // targetWidth: 100,
-       // targetHeight: 100,
-       // popoverOptions: CameraPopoverOptions,
-       // saveToPhotoAlbum: false,
-       // correctOrientation:true
+        encodingType: Camera.EncodingType.JPEG
     };
-
-    /*$ionicPlatform.ready(function () {
-
-    });*/
 }])
 ;
 
